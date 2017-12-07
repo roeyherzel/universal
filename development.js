8e2f71cbeca794ab2a9d391939d150153545f94e
roey <rherzel@treetopllc.com>
2017-12-06 17:41:11 -0800

    HMR worksgit status

--- development.js
+++ development.js
@@ -1,15 +1,22 @@
+const morgan = require('morgan');
 const express = require('express');
 const path = require('path');
-const app = express();
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const webpackHotServerMiddleware = require('webpack-hot-server-middleware');
 const config = require('./webpack.development.config.js');
+
+
+const app = express();
+
+app.use(morgan('dev'));
+
 const compiler = webpack(config);
 
 app.use(webpackDevMiddleware(compiler, {
 	publicPath: "/static/",
+	stats: 'minimal',
 }));
 
 app.use(webpackHotMiddleware(compiler.compilers.find(compiler => compiler.name === 'client')));
--- webpack.development.config.js
+++ webpack.development.config.js
@@ -2,107 +2,109 @@ const path = require('path');
 const webpack = require('webpack');
 
 
-module.exports = [
-	{
-		name: 'client',
-		target: 'web',
-		entry: [
-			'react-hot-loader/patch',
-			'webpack-hot-middleware/client',
-			'./client.jsx'
-		],
-		output: {
-			path: path.join(__dirname, 'static'),
-			filename: 'client.js',
-			publicPath: '/static/',
-		},
-		resolve: {
-			extensions: ['.js', '.jsx']
-		},
-		devtool: 'source-map',
-		module: {
-			rules: [
-				{
-					test: /\.(js|jsx)$/,
-					exclude: /(node_modules\/)/,
-					use: [
-						{
-							loader: 'babel-loader',
-						}
-					]
-				},
-				{
-					test: /\.scss$/,
-					use: [
-						{
-							loader: 'style-loader',
-						},
-						{
-							loader: 'css-loader',
-							options: {
-								modules: true,
-								importLoaders: 1,
-								localIdentName: '[name]__[local]___[hash:base64:5]',
-								sourceMap: true
-							}
-						},
-						{
-							loader: 'sass-loader'
-						}
-					]
-				}
-			],
-		},
-		plugins: [
-			new webpack.HotModuleReplacementPlugin(),
-		]
+const client = {
+	name: 'client',
+	target: 'web',
+	entry: [
+		'react-hot-loader/patch',
+		'webpack-hot-middleware/client',
+		'./client.jsx'
+	],
+	output: {
+		path: path.join(__dirname, 'static'),
+		filename: 'client.js',
+		publicPath: '/static/',
 	},
-	{
-		name: 'server',
-		target: 'node',
-		entry: './server.jsx',
-		output: {
-			path: path.join(__dirname, 'static'),
-			filename: 'server.js',
-			libraryTarget: 'commonjs2',
-			publicPath: '/static/',
-		},
-		devtool: 'source-map',
-		resolve: {
-			extensions: ['.js', '.jsx']
-		},
-		module: {
-			rules: [
-				{
-					test: /\.(js|jsx)$/,
-					exclude: /(node_modules\/)/,
-					use: [
-						{
-							loader: 'babel-loader',
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules\/)/,
+				use: [
+					{
+						loader: 'babel-loader',
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'style-loader',
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+							importLoaders: 1,
+							localIdentName: '[name]__[local]___[hash:base64:5]',
+							sourceMap: true
 						}
-					]
-				},
-				{
-					test: /\.scss$/,
-					use: [
-						{
-							loader: 'isomorphic-style-loader',
-						},
-						{
-							loader: 'css-loader',
-							options: {
-								modules: true,
-								importLoaders: 1,
-								localIdentName: '[name]__[local]___[hash:base64:5]',
-								sourceMap: true
-							}
-						},
-						{
-							loader: 'sass-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
+		],
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+	]
+};
+
+
+const server = {
+	name: 'server',
+	target: 'node',
+	entry: './server.jsx',
+	output: {
+		path: path.join(__dirname, 'static'),
+		filename: 'server.js',
+		libraryTarget: 'commonjs2',
+		publicPath: '/static/',
+	},
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules\/)/,
+				use: [
+					{
+						loader: 'babel-loader',
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'isomorphic-style-loader',
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+							importLoaders: 1,
+							localIdentName: '[name]__[local]___[hash:base64:5]',
+							sourceMap: true
 						}
-					]
-				}
-			],
-		},
-	}
-];
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
+		],
+	},
+};
+
+module.exports = [client, server];
